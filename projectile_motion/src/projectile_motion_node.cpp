5c30e49d0cf7b7ca12f1c580aa2935657ee91766
冯云龙 <ericc_feng@qq.com>
2023-04-11 03:08:44 +0800

    feat: Add projectile_motion package

--- projectile_motion/src/projectile_motion_node.cpp
+++ projectile_motion/src/projectile_motion_node.cpp
@@ -7,7 +7,7 @@ namespace projectile_motion
 {
 
 ProjectileMotionNode::ProjectileMotionNode(rclcpp::NodeOptions options)
-  : Node("projectile_motion_node", options.use_intra_process_comms(true))
+: Node("projectile_motion_node", options.use_intra_process_comms(true))
 {
   offset_x_ = this->declare_parameter("projectile.offset_x", 0.0);
   offset_y_ = this->declare_parameter("projectile.offset_y", 0.0);
@@ -22,32 +22,32 @@ ProjectileMotionNode::ProjectileMotionNode(rclcpp::NodeOptions options)
   solver_type_ = this->declare_parameter("projectile.solver_type", "gravity");
   target_frame_ = this->declare_parameter("projectile.target_frame", "shooter_link");
 
-  if (solver_type_ == "gravity")
-  {
+  if (solver_type_ == "gravity") {
     solver_ = std::make_shared<rmoss_projectile_motion::GravityProjectileSolver>(shoot_speed_);
-  }
-  else if (solver_type_ == "gaf")
-  {
+  } else if (solver_type_ == "gaf") {
     friction_ = this->declare_parameter("projectile.friction", 0.001);
-    solver_ = std::make_shared<rmoss_projectile_motion::GafProjectileSolver>(shoot_speed_, friction_);
-  }
-  else
-  {
+    solver_ =
+      std::make_shared<rmoss_projectile_motion::GafProjectileSolver>(shoot_speed_, friction_);
+  } else {
     RCLCPP_ERROR(this->get_logger(), "Unknown solver type: %s", solver_type_.c_str());
     return;
   }
 
-  gimbal_cmd_publisher_ = this->create_publisher<rm_interfaces::msg::GimbalCmd>(gimbal_cmd_topic_, 10);
+  gimbal_cmd_publisher_ = this->create_publisher<rm_interfaces::msg::GimbalCmd>(
+    gimbal_cmd_topic_,
+    10);
   shoot_data_subscriber_ = this->create_subscription<rm_interfaces::msg::RobotShootData>(
-      shoot_data_topic_, 10, std::bind(&ProjectileMotionNode::shoot_data_callback, this, std::placeholders::_1));
+    shoot_data_topic_, 10,
+    std::bind(&ProjectileMotionNode::shoot_data_callback, this, std::placeholders::_1));
 
   tf_buffer_ = std::make_shared<tf2_ros::Buffer>(
-      this->get_clock(), rclcpp::Duration::from_seconds(10.0).to_chrono<std::chrono::nanoseconds>());
+    this->get_clock(), rclcpp::Duration::from_seconds(10.0).to_chrono<std::chrono::nanoseconds>());
   tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
   target_subscriber_.subscribe(this, target_topic_, rmw_qos_profile_sensor_data);
-  tf_filter_ = std::make_shared<tf2_filter>(target_subscriber_, *tf_buffer_, target_frame_, 10,
-                                            this->get_node_logging_interface(), this->get_node_clock_interface(),
-                                            std::chrono::duration<int>(1));
+  tf_filter_ = std::make_shared<tf2_filter>(
+    target_subscriber_, *tf_buffer_, target_frame_, 10,
+    this->get_node_logging_interface(), this->get_node_clock_interface(),
+    std::chrono::duration<int>(1));
   tf_filter_->registerCallback(&ProjectileMotionNode::target_callback, this);
 }
 
@@ -71,27 +71,27 @@ void ProjectileMotionNode::target_callback(const auto_aim_interfaces::msg::Targe
   // Transform the pitch and yaw to the shooter_link frame.
   // Define the transform from the target frame to the shooter_link frame.
   geometry_msgs::msg::TransformStamped transform_stamped;
-  try
-  {
-    transform_stamped = tf_buffer_->lookupTransform(target_frame_, msg->header.frame_id, target_time);
-  }
-  catch (tf2::TransformException& ex)
-  {
+  try {
+    transform_stamped =
+      tf_buffer_->lookupTransform(target_frame_, msg->header.frame_id, target_time);
+  } catch (tf2::TransformException & ex) {
     RCLCPP_ERROR(this->get_logger(), "Failed to transform target pose: %s", ex.what());
     return;
   }
 
   // Create a quaternion from the pitch and yaw angles.
   Eigen::Quaterniond q = Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()) *
-                         Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
+    Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
 
   // Transform the quaternion to the shooter_link frame.
   Eigen::Quaterniond q_shooter_link =
-      Eigen::Quaterniond(transform_stamped.transform.rotation.w, transform_stamped.transform.rotation.x,
-                         transform_stamped.transform.rotation.y, transform_stamped.transform.rotation.z) *
-      q *
-      Eigen::Quaterniond(transform_stamped.transform.rotation.w, -transform_stamped.transform.rotation.x,
-                         -transform_stamped.transform.rotation.y, -transform_stamped.transform.rotation.z);
+    Eigen::Quaterniond(
+    transform_stamped.transform.rotation.w, transform_stamped.transform.rotation.x,
+    transform_stamped.transform.rotation.y, transform_stamped.transform.rotation.z) *
+    q *
+    Eigen::Quaterniond(
+    transform_stamped.transform.rotation.w, -transform_stamped.transform.rotation.x,
+    -transform_stamped.transform.rotation.y, -transform_stamped.transform.rotation.z);
 
   // Extract the pitch and yaw angles from the transformed quaternion.
   Eigen::Vector3d euler_angles = q_shooter_link.toRotationMatrix().eulerAngles(2, 1, 0);
@@ -109,19 +109,16 @@ void ProjectileMotionNode::target_callback(const auto_aim_interfaces::msg::Targe
   gimbal_cmd_publisher_->publish(gimbal_cmd);
 }
 
-void ProjectileMotionNode::shoot_data_callback(const rm_interfaces::msg::RobotShootData::SharedPtr msg)
+void ProjectileMotionNode::shoot_data_callback(
+  const rm_interfaces::msg::RobotShootData::SharedPtr msg)
 {
   shoot_speed_ = static_cast<double>(msg->bullet_speed);
-  if (solver_type_ == "gravity")
-  {
+  if (solver_type_ == "gravity") {
     solver_ = std::make_shared<rmoss_projectile_motion::GravityProjectileSolver>(shoot_speed_);
-  }
-  else if (solver_type_ == "gaf")
-  {
-    solver_ = std::make_shared<rmoss_projectile_motion::GafProjectileSolver>(shoot_speed_, friction_);
-  }
-  else
-  {
+  } else if (solver_type_ == "gaf") {
+    solver_ =
+      std::make_shared<rmoss_projectile_motion::GafProjectileSolver>(shoot_speed_, friction_);
+  } else {
     RCLCPP_ERROR(this->get_logger(), "Unknown solver type: %s", solver_type_.c_str());
     return;
   }
