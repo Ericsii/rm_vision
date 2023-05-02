c42c8a01ffb72e8744294430c69860eaf5ef0705
Ericsii <ericc_feng@qq.com>
2023-05-02 22:48:17 +0800

    feat: Create package

---
+++ rm_omni/include/rm_omni/omni_node.hpp
@@ -0,0 +1,16 @@
+#ifndef RM_OMNI__OMNI_NODE_HPP_
+#define RM_OMNI__OMNI_NODE_HPP_
+
+#include <rclcpp>
+
+
+namespace rm_omni
+{
+
+class OmniNode : public rclcpp::Node
+{
+};
+
+}  // namespace rm_omni
+
+#endif  // RM_OMNI__OMNI_NODE_HPP_
